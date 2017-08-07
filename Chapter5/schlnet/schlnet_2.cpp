57d0a7c9d5e0be0ab579ad0223816b56f1fb0b45
suzyz <suzyzhang0@gmail.com>
2017-08-07 18:36:41 +0800

    backup on 20170807.

--- Chapter5/schlnet/schlnet_2.cpp
+++ Chapter5/schlnet/schlnet_2.cpp
@@ -13,37 +13,14 @@ using namespace std;
 const int maxn = 102;
 
 int n;
-bool g[maxn][maxn],vis[maxn];
-int in_degree[maxn],topo[maxn];
+bool g[maxn][maxn];
 
 int top=0,stack[maxn];
 bool in_satck[maxn];
 int idx=0,dfn[maxn],low[maxn];
 
 int tot=0,color[maxn];
-int out_degree[maxn];
-
-void topo_dfs(int cur)
-{
-	vis[cur]=true;
-	for(int i=1;i<=n;i++)
-		if(g[cur][i] && !vis[i])
-		{
-			in_degree[i]--;
-			topo_dfs(i);
-		}
-
-	topo[0]++;
-	topo[topo[0]]=cur;
-}
-
-void dfs(int cur)
-{
-	vis[cur]=true;
-	for(int i=1;i<=n;i++)
-		if(g[cur][i] && !vis[i])
-			dfs(i);
-}
+int in_degree[maxn],out_degree[maxn];
 
 void tarjan(int cur)
 {
@@ -94,36 +71,11 @@ int main()
 			if(t==0) break;
 			g[i][t]=true;
 		}
-	
-	for(int i=1;i<=n;i++)
-		if(!vis[i])
-			topo_dfs(i);
-
-	memset(vis,0,sizeof(vis));
-
-	int ans1=0;
-	for(int i=n;i>=1;i--)
-		if(!vis[topo[i]])
-		{
-			ans1++;
-			dfs(topo[i]);
-		}
-
-	fout<<ans1<<endl;
 
 	for(int i=1;i<=n;i++)
 		if(dfn[i]==0)
 			tarjan(i);
 
-	if(tot==1)
-	{
-		fout<<0<<endl;
-		fin.close();
-		fout.close();
-		return 0;
-	}
-
-	memset(in_degree,0,sizeof(in_degree));
 	for(int i=1;i<=n;i++)
 		for(int j=1;j<=n;j++)
 			if(g[i][j] && color[i]!=color[j])
@@ -132,14 +84,21 @@ int main()
 				out_degree[color[i]]++;
 			}
 
-	int sum1=0,sum2=0;
+	int ans1=0,ans2=0;
 	for(int i=1;i<=tot;i++)
 	{
-		if(in_degree[i]==0) sum1++;
-		if(out_degree[i]==0) sum2++;
+		if(in_degree[i]==0) ans1++;
+		if(out_degree[i]==0) ans2++;
 	}
-	if(sum1<sum2) sum1=sum2;
-	fout<<sum1<<endl;
+
+	fout<<ans1<<endl;
+	if(tot==1)
+		fout<<0<<endl;
+	else
+	{
+		ans2=ans2>ans1 ? ans2:ans1;
+		fout<<ans2<<endl;
+	}
 
 	fin.close();
 	fout.close();
