a1cf74b11d68b368100262b0e7bed4283f2aa057
suzyz <suzyzhang0@gmail.com>
2017-08-17 18:52:20 +0800

    backup on 20170817.

--- Chapter6/prime3/prime3.cpp
+++ Chapter6/prime3/prime3.cpp
@@ -1,14 +1,21 @@
 /*
 ID: suzyzha0
-PROG: prime2
+PROG: prime3
 LANG: C++
 */
 
 #include <iostream>
 #include <fstream>
-#include <cstring>
+#include <algorithm>
+#include <ctime>
 
 using namespace std;
+const int maxp=100000;
+const int full=(1<<10)-1;
+
+int change[12];
+bool is_prime[maxp+20];
+int next_digit[maxp+20];
 
 int sum,ans_count=0;
 struct Square
@@ -16,121 +23,224 @@ struct Square
 	int d[5][5];
 } sq,ans[1000];
 
-int row_count[5],col_count[5];
+int row[5],col[5],diag[2];
 
-const int order[][2] = 	{ {0,1},{0,2},{0,3},{0,4},
-					{1,0},{1,1},{1,2},{1,3},{1,4},
-					{2,0},{2,1},{2,2},{2,3},{2,4},
-					{3,0},{3,1},{3,2},{3,3},{3,4},
-					{4,0},{4,1},{4,2},{4,3},{4,4}};
-
-const int pos[2][5][2] = {{ {0,0},{1,1},{2,2},{3,3},{4,4} },
-						  { {4,0},{3,1},{2,2},{1,3},{0,4} }};
-
-bool is_prime(int d)
+int get_sum(int cur)
 {
-	if((d&1)==0) return false;
-	
-}
-
-bool check_row(int cur)
-{
-	if(row_count[cur]!=sum) return false;
-	if(!is_prime(row[cur])) return false;
-
-	return true;
-}
-
-bool check_col(int cur)
-{
-	if(col_count[cur]!=sum) return false;
-	if(!is_prime(col[cur])) return false;
-
-	return true;
-}
-
-bool check_diag()
-{
-	int s,d;
-	for(int i=0;i<2;i++)
+	int res=0;
+	while(cur>0)
 	{
-		s=0;
-		for(int j=0;j<5;j++)
-		{
-			s+=sq.d[pos[i][j][0]][pos[i][j][1]];
-			d=10*d+sq.d[pos[i][j][0]][pos[i][j][1]];
-		}
-
-		if(s!=sum || !is_prime(d)) return false;
+		res+=cur%10;
+		cur/=10;
 	}
-	return true;
+	return res;
 }
 
-void dfs(int cur)
+void get_primes()
 {
-	if(cur>=24)
-	{
-		if(check_diag())
+	for(int i=3;i<maxp;i+=2) is_prime[i]=true;
+
+	int tmp;
+	for(int i=3;i<maxp;i+=2)
+		if(is_prime[i])
 		{
-			ans[ans_count]=sq;
-			ans_count++;
+			tmp=i<<1;
+			if(tmp>=maxp) break;
+			while(tmp<maxp)
+			{
+				is_prime[tmp]=false;
+				tmp+=i;
+			}
 		}
+
+	for(int i=10001;i<maxp;i+=2)
+		if(is_prime[i] && get_sum(i)==sum)
+			next_digit[i]=1;
+
+	int st=1000,en=10000;
+	for(int i=3;i>=0;i--)
+	{
+		for(int j=st;j<en;j++)
+			for(int k=0;k<10;k++)
+				if(next_digit[j*10+k]!=0)
+					next_digit[j]|=(1<<k);
+		st/=10;
+		en/=10;
+	}
+}
+
+void record()
+{
+	for(int i=0;i<4;i++)
+	{
+		int x=0;
+		for(int j=0;j<4;j++)
+			x+=sq.d[i][j];
+
+		sq.d[i][4]=sum-x;
+	}
+
+	for(int j=0;j<5;j++)
+	{
+		int x=0;
+		for(int i=0;i<5;i++)
+			x+=sq.d[i][j];
+
+		sq.d[4][j]=sum-x;
+	}
+
+	ans[ans_count]=sq;
+	ans_count++;
+}
+
+void dfs(int x,int y)
+{
+	//printf("%d %d\n", x,y);
+	if(x>3)
+	{
+		record();
 		return;
 	}
+	int hash=full,hash1=full,hash2=full;
 
-	int x=order[i][0],y=order[i][1];
-
-	for(int i=0;i<10;i++)
+	if(y==0) /* row x-1 has been searched, so calculate (x-1,4) */
 	{
-		if(i>sum) break;
-		if((x==0 || y==0) && i==0) continue;
-		if((x==4 || y==4) && i%2==0) continue;
-
-		sq[x][y]=i;
-		flag=true;
-
-		row_count[x]+=i;
-		col_count[y]+=i;
-		row[x]=10*row[x]+i;
-		col[y]=10*col[y]+i;
-		if(y==4) flag=check_row(x);
-		if(x==4) flag=check_col(y);
-
-		if(flag) dfs(cur+1);
-
-		row_count[x]-=i;
-		col_count[y]-=i;
-		row[x]=(row[x]-i)/10;
-		col[y]=(row[x]-i)/10;
+		sq.d[x-1][4]=change[next_digit[row[x-1]]];
+		col[4]=10*col[4]+sq.d[x-1][4];
 	}
+	else
+	{
+		hash &= next_digit[row[x]];
+		if(x==3) /* calculate (4,y-1) */
+		{
+			//sq.d[4][y-1]=change[next_digit[col[y-1]]];
+			row[4]=10*row[4]+change[next_digit[col[y-1]]];
+			hash1 &= next_digit[row[4]];
+		}
+	}
+
+	if(x>0)
+	{
+		hash &= next_digit[col[y]]; /* this column */
+		if(y==3) hash2 &= next_digit[col[4]]; /* next column ?? */
+	}
+
+	if(x==y)
+	{
+		hash &= next_digit[diag[0]];
+		//diag[0]*=10;
+	}
+
+	if(x==1 && y==0)
+		diag[1]=change[next_digit[row[0]]];
+
+	if(x+y==4)
+	{
+		hash &= next_digit[diag[1]];
+		//diag[1]*=10;
+	}
+
+	// row[x]*=10;
+	// col[y]*=10;
+
+	while(hash!=0)
+	{
+		int cur=hash&(-hash);
+		int i=change[cur];
+		printf("cur:%d i:%d\n",cur,i);
+
+		if((hash1 & next_digit[col[y]+i]) && (hash2 & next_digit[row[x]+i]))
+		{
+			sq.d[x][y]=i;
+
+			row[x]=10*row[x]+i;
+			col[y]=10*col[y]+i;
+			if(x==y) diag[0]=10*diag[0]+i;
+			if(x+y==4) diag[1]=10*diag[1]+i;
+
+			// row[x]+=i;
+			// col[y]+=i;
+			// if(x==y) diag[0]+=i;
+			// if(x+y==4) diag[1]-=i;
+
+			if(y<3)
+				dfs(x,y+1);
+			else
+				dfs(x+1,0);
+
+			row[x]/=10;
+			col[y]/=10;
+			if(x==y) diag[0]/=10;
+			if(x+y==4) diag[1]/=10;
+
+			// row[x]-=i;
+			// col[y]-=i;
+			// if(x==y) diag[0]-=i;
+			// if(x+y==4) diag[1]-=i;
+		}
+
+		hash-=cur;
+	}
+	// row[x]/=10;
+	// col[y]/=10;
+	// if(x==y) diag[0]/=10;
+	if(y==0) col[4]/=10;
+	if(x==3) row[4]/=10;
+	// if(x+y==4) diag[1]/=10;
+}
+
+bool my_cmp(const Square &a,const Square &b)
+{
+	for(int i=0;i<5;i++)
+		for(int j=0;j<5;j++)
+		{
+			if(a.d[j][i]<b.d[j][i]) return true;
+			if(a.d[j][i]>b.d[j][i]) return false;
+		}
+	return false;
 }
 
 int main()
 {
-	fstream fin("prime2.in",ios::in);
-	fstream fout("prime2.out",ios::out);
+	clock_t begin=clock();
+
+	fstream fin("prime3.in",ios::in);
+	fstream fout("prime3.out",ios::out);
 
 	fin>>sum>>sq.d[0][0];
 
-	row_count[0]=col_count[0]=row[0]=col[0]=sq.d[0][0];
+	get_primes();
 
-	dfs(0);
+	diag[1]=0;
+	row[0]=col[0]=diag[0]=sq.d[0][0];
+
+	for(int i=0;i<10;i++)
+		change[1<<i]=i;
+
+	dfs(0,1);
 
 	if(ans_count==0)
 		fout<<"NONE"<<endl;
 	else
+	{
+		sort(ans,ans+ans_count,my_cmp);
 		for(int i=0;i<ans_count;i++)
 		{
 			if(i>0) fout<<endl;
 			for(int j=0;j<5;j++)
 			{
 				for(int k=0;k<5;k++)
-					fout<<ans[cur].d[j][k];
+					fout<<ans[i].d[k][j];
 				fout<<endl;
 			}
 		}
+	}
 
 	fin.close();
 	fout.close();
+
+	clock_t en=clock();
+	printf("%d\n", ans_count);
+	printf("time:%lu\n", (en-begin)*1000/CLOCKS_PER_SEC);
 	return 0;
 }
